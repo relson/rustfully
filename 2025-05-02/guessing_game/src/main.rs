391e0063082808703bad2a88440b8b33db0d98bc
Sebastião Relson <relsonto@gmail.com>
2025-05-02 09:22:19 -0300

    2025-05-02 Coding your fist project in Rust

---
+++ 2025-05-02/guessing_game/src/main.rs
@@ -0,0 +1,35 @@
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        std::io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        if guess < secret_number {
+            println!("Too small!");
+        } else if guess > secret_number {
+            println!("Too big!");
+        } else {
+            println!("You win!");
+            break;
+        }
+    }
+    println!("The secret number was: {}", secret_number);
+    println!("Thanks for playing!");
+}
